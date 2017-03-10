d763e2757fdd189e7e856846a4af9226259ddc62
avel123 <[valen_lj@hotmail.com]>
2017-03-09 19:47:08 -0500

    2.2
    Se agrego funcion de registrar categoría

--- core/core.php
+++ core/core.php
@@ -9,10 +9,11 @@
 
 
   define('HTML_DIR', 'html/');
+  define('MODEL_DIR', 'core/modelos/');
   define('APP_TITLE', 'CS | Página Principal | CS');
   define('APP_COPY', 'Copyright &copy; ' .date('Y', time()) . ' Capricorn Strorage.');
 
-  require('core/models/class.Conexion.php');
+  require(MODEL_DIR.'class.Conexion.php');
   require('core/bin/functions/Encrypt.php');
 
 
